8c305278dc495509a9f766166688c0331cbaa688
rayboi69 <raulreyaso@gmail.com>
2015-02-01 21:18:43 -0600

    Add Google Maps SDK framework and test Map

--- CityAudioTour/AppDelegate.swift
+++ CityAudioTour/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyAkqc9SW297lf1CgXgOStMdB4om4jT3g3Q");
+        
         return true
     }
 
--- CityAudioTour/ViewController.swift
+++ CityAudioTour/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
