114185d039f3576f15909dc954dced141828a96b
koke6645 <koke6645@gmail.com>
2015-02-12 14:34:50 -0600

    Red: I merge my storyboard and text to speech view

---
+++ CityAudioTour/TextToSpeechViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TextToSpeechViewController.swift
+//  CityAudioTour
+//
+//  Created by Red_iMac on 2/12/15.
+//  Copyright (c) 2015 SE491-591. All rights reserved.
+//
+
+import UIKit
+
+class TextToSpeechViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
