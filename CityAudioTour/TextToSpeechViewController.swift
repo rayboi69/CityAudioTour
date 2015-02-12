236a73af53da578ea55a1e08eabdee81e5fc5fba
koke6645 <koke6645@gmail.com>
2015-02-12 16:48:39 -0600

    Red: Add SwiftyJSON and retrieve content from server

--- CityAudioTour/TextToSpeechViewController.swift
+++ CityAudioTour/TextToSpeechViewController.swift
@@ -7,21 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TextToSpeechViewController: UIViewController {
+    
+    var receiveID : Int?
+    var synthersizer = AVSpeechSynthesizer()
+    var utterance = AVSpeechUtterance(string: "")
+    
+    @IBOutlet weak var attractionLabel: UILabel!
+    
+    @IBOutlet weak var speechContent: UITextView!
+    
+    @IBAction func playAudio(sender: UIButton) {
+        if !synthersizer.continueSpeaking() {
+            utterance = AVSpeechUtterance(string: speechContent.text)
+            synthersizer.speakUtterance(utterance)
+        }
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        synthersizer.stopSpeakingAtBoundary(.Immediate)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        synthersizer.speakUtterance(utterance)
+        
+        //Hard code, waittingto connect to server
+        receiveID = 2
+        
+        //Get content from server
+        self.retrieveDataFromServer()
     }
     
-
+    func retrieveDataFromServer() {
+        let subURL = "http://cityaudiotourweb.azurewebsites.net/api/attraction/"
+        let url = NSURL(string: "\(subURL)\(receiveID!)/contents")
+        var request = NSURLRequest(URL: url!)
+        var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
+        if data != nil {
+            var content = JSON(data: data!)
+            
+            var tit = content[1]["Title"]
+            attractionLabel.attributedText = NSAttributedString(string: "\(tit)")
+            
+            var con = content[1]["Description"]
+            speechContent.attributedText = NSMutableAttributedString(string: "\(con)")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
