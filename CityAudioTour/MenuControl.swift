3800b31b6405f577337b6af4c8fe76623d424ee9
Pichan Vasantakitkumjorn <pete@lpc-232-212.lpc-wireless.depaul.edu>
2015-02-22 14:25:13 -0600

    flyout menu updated.

---
+++ CityAudioTour/MenuControl.swift
@@ -0,0 +1,9 @@
+//
+//  MenuControl.swift
+//  CityAudioTour
+//
+//  Created by Pichan Vasantakitkumjorn on 2/22/15.
+//  Copyright (c) 2015 SE491-591. All rights reserved.
+//
+
+import Foundation
