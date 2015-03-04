5bb73d7a64e0f45e6d215ac3313b82bc64b35df7
drandx <garjuanpablo@gmail.com>
2015-03-03 21:05:06 -0600

    Remove fake data from ClassificationModel

--- CityAudioTour/ClassificationModel.swift
+++ CityAudioTour/ClassificationModel.swift
@@ -49,30 +49,10 @@ class ClassificationModel {
     }
     
     func LoadCategoryAndTagFromServer() {
-        /*
+        
         categoryList = self.server?.GetCategoryList()
         tagList = self.server?.GetTagList()
-        */
-        //Useing fake data
-        self.getFakeData()
+      
     }
     
-    func getFakeData() {
-        categoryList?.append(Category(id: 1, name: "Category: 1"))
-        categoryList?.append(Category(id: 2, name: "Category: 2"))
-        categoryList?.append(Category(id: 3, name: "Category: 3"))
-        categoryList?.append(Category(id: 4, name: "Category: 4"))
-        
-        tagList?.append(Tag(id: 1, name: "Tag: 1"))
-        tagList?.append(Tag(id: 2, name: "Tag: 2"))
-        tagList?.append(Tag(id: 3, name: "Tag: 3"))
-        tagList?.append(Tag(id: 4, name: "Tag: 4"))
-        tagList?.append(Tag(id: 5, name: "Tag: 5"))
-        tagList?.append(Tag(id: 6, name: "Tag: 6"))
-        tagList?.append(Tag(id: 7, name: "Tag: 7"))
-        tagList?.append(Tag(id: 8, name: "Tag: 8"))
-        tagList?.append(Tag(id: 9, name: "Tag: 9"))
-        tagList?.append(Tag(id: 10, name: "Tag: 10"))
-    }
-
 }
