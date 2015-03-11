0a738ce418cf2095ead73c10295d32f2f92f3459
koke6645 <koke6645@gmail.com>
2015-03-10 20:04:43 -0500

    Red: show only select attraction on map

--- CityAudioTour/AttractionListTableViewController.swift
+++ CityAudioTour/AttractionListTableViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class AttractionListTableViewController: UITableViewController {
 
-    var attractions: [Attraction]?
+    var attractions: [Attraction]! = AttractionsModel.sharedInstance.attractionsList
     var sectionTitle = "Attraction List"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        attractions = AttractionsModel.sharedInstance.attractionsList
     }
 
     // MARK: - Table view data source
@@ -57,48 +56,20 @@ class AttractionListTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let attraction = attractions![indexPath.row]
+        AttractionsModel.sharedInstance.selectAttractions = [attraction]
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ListToDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow() {
+            let cell = sender as UITableViewCell
+            if let indexPath = tableView.indexPathForCell(cell) {
                 let detailScene = segue.destinationViewController as DetailViewController
                 detailScene.receiveID = attractions![indexPath.row].AttractionID
             }
