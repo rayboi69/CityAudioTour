f6165511f4f5f0ed8e862b2c24c5e1bbddd3f525
Juan Garcia <garjuanpablo@gmail.com>
2015-04-15 13:15:36 -0500

    Added files for authentication functionality.

---
+++ CityAudioTour/SignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.swift
+//  CityAudioTour
+//
+//  Created by Juan Garcia on 4/15/15.
+//  Copyright (c) 2015 SE491-591. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+}
