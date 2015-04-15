0fa886a686823f271a1d551c998fa96d6f6cfe84
koke6645 <koke6645@gmail.com>
2015-04-14 22:49:45 -0500

    Red: Add an add to my route button in attraction list view

--- CityAudioTour/AttractionListTableViewController.swift
+++ CityAudioTour/AttractionListTableViewController.swift
@@ -13,7 +13,8 @@ class AttractionListTableViewController: UITableViewController {
     var sectionTitle = "Attraction List"
     private var _routesManager = RoutesManager.sharedInstance
     private var _attractionsManager = AttractionsManager.sharedInstance
-    private var _attractions = [Attraction]()
+    private var _selectAttractionsManager = SelectAttractionsManager.sharedInstance
+    private var attractions = [Attraction]()
     
     // MARK: - View Controller Lifecycle
 
@@ -22,7 +23,7 @@ class AttractionListTableViewController: UITableViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-        _attractions = _attractionsManager.attractionsList
+        attractions = _attractionsManager.attractionsList
         tableView.reloadData()
     }
     
@@ -38,12 +39,12 @@ class AttractionListTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-            return _attractions.count
+            return attractions.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("AttractionCell", forIndexPath: indexPath) as UITableViewCell
-        let attraction = _attractions[indexPath.row]
+        let attraction = attractions[indexPath.row]
         cell.textLabel?.text = attraction.AttractionName
         return cell
     }
@@ -52,10 +53,31 @@ class AttractionListTableViewController: UITableViewController {
         return sectionTitle
     }
     
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
+        var addToMyRoute = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Add" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            
+            let alert = UIAlertController(title: nil, message: "\(self.attractions[indexPath.row].AttractionName) has been added to My Route", preferredStyle: .ActionSheet)
+            let okAction = UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+            self.editing = false
+            self._selectAttractionsManager.addAttraction(self.attractions[indexPath.row].AttractionID)
+            self.presentViewController(alert, animated: true, completion: nil)
+        })
+        
+        addToMyRoute.backgroundColor = UIColor.blueColor()
+        
+        return [addToMyRoute]
+    }
+    
     // MARK: - Navigation
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        var attraction = _attractions[indexPath.row]
+        var attraction = attractions[indexPath.row]
         var route = Route()
         route.AttractionIDs = [attraction.AttractionID]
         route.CategoriesIDs = [attraction.CategoryID]
@@ -69,7 +91,7 @@ class AttractionListTableViewController: UITableViewController {
             let cell = sender as UITableViewCell
             if let indexPath = tableView.indexPathForCell(cell) {
                 let detailScene = segue.destinationViewController as DetailViewController
-                detailScene.receiveID = _attractions[indexPath.row].AttractionID
+                detailScene.receiveID = attractions[indexPath.row].AttractionID
             }
         }
     }
