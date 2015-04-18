ae252a3af9aa813fcd16761b911c6cc20f19bcc8
peteac119 <pichanv@gmail.com>
2015-04-17 19:40:22 -0500

    comment some test cases out.

--- CityAudioTourTests/ContentViewTests.swift
+++ CityAudioTourTests/ContentViewTests.swift
@@ -12,14 +12,14 @@ import SwiftyJSON
 
 class ContentViewTests: XCTestCase {
     
-    var service: CATAzureService!
-    var contentView:AudioTourViewController!
+//    var service: CATAzureService!
+//    var contentView:AudioTourViewController!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        service = CATAzureService()
-        contentView = AudioTourViewController()
+//        service = CATAzureService()
+//        contentView = AudioTourViewController()
     }
     
     override func tearDown() {
@@ -27,70 +27,70 @@ class ContentViewTests: XCTestCase {
         super.tearDown()
     }
     
-    func testContentBasedOnAttractionID(){
-        class Content{
-            var ID = 0
-            var description:String = ""
-            var title:String = ""
-        }
-        
-        //Get all attractions first
-        var attractList:[Attraction] = service.GetAttractions()
-        //If it is zero, it can't get data. Test failed.
-        if attractList.isEmpty {
-            XCTFail("Can't get data with some reason")
-        }
-        
-        var expectation:[Content] = [Content]()
-        
-        for attractID in attractList{
-            let expect:XCTestExpectation = expectationWithDescription("Get AttractionContent of " + String(attractID.AttractionID))
-            service.GetAttractionContentByID(attractID.AttractionID, MainThread: NSOperationQueue(), handler: {(respond:NSURLResponse!,data:NSData!,error:NSError!) -> Void in
-                if data != nil{
-                    var json = JSON(data: data!)
-                    var title = json[1]["Title"].stringValue
-                    var speechText = json[1]["Description"].stringValue
-                    let content:Content = Content()
-                    content.title = title
-                    content.description = speechText
-                    content.ID = attractID.AttractionID
-                    
-                    expectation.append(content)
-                    
-                    expect.fulfill()
-                }else{
-                    XCTFail("Can't get data with some reason")
-                }
-            })
-            waitForExpectationsWithTimeout(5, handler: nil)
-        }
-        
-        if expectation.count == 0 {
-            XCTFail("Can't get data with some reason")
-        }
-        
-        for attractID in expectation{
-            let expect:XCTestExpectation = expectationWithDescription("Get AttractionContent of " + String(attractID.ID))
-            let content:Content = Content()
-            contentView.receiveID = attractID.ID
-//            contentView.handler = {(response:NSURLResponse!,data:NSData!,error:NSError!) -> Void in
-//                if data != nil {
+//    func testContentBasedOnAttractionID(){
+//        class Content{
+//            var ID = 0
+//            var description:String = ""
+//            var title:String = ""
+//        }
+//        
+//        //Get all attractions first
+//        var attractList:[Attraction] = service.GetAttractions()
+//        //If it is zero, it can't get data. Test failed.
+//        if attractList.isEmpty {
+//            XCTFail("Can't get data with some reason")
+//        }
+//        
+//        var expectation:[Content] = [Content]()
+//        
+//        for attractID in attractList{
+//            let expect:XCTestExpectation = expectationWithDescription("Get AttractionContent of " + String(attractID.AttractionID))
+//            service.GetAttractionContentByID(attractID.AttractionID, MainThread: NSOperationQueue(), handler: {(respond:NSURLResponse!,data:NSData!,error:NSError!) -> Void in
+//                if data != nil{
 //                    var json = JSON(data: data!)
-//                    content.title = json[1]["Title"].stringValue
-//                    content.description = json[1]["Description"].stringValue
+//                    var title = json[1]["Title"].stringValue
+//                    var speechText = json[1]["Description"].stringValue
+//                    let content:Content = Content()
+//                    content.title = title
+//                    content.description = speechText
+//                    content.ID = attractID.AttractionID
+//                    
+//                    expectation.append(content)
+//                    
 //                    expect.fulfill()
 //                }else{
-//                    XCTFail("Can't get data with some reason.")
+//                    XCTFail("Can't get data with some reason")
 //                }
-//            }
-            
-            contentView.viewDidLoad()
-            waitForExpectationsWithTimeout(5, handler: nil)
-            
-            XCTAssertEqual(attractID.description, content.description, "Same description")
-            XCTAssertEqual(attractID.title, content.title, "Same Title")
-        }
-    }
+//            })
+//            waitForExpectationsWithTimeout(5, handler: nil)
+//        }
+//        
+//        if expectation.count == 0 {
+//            XCTFail("Can't get data with some reason")
+//        }
+//        
+//        for attractID in expectation{
+//            let expect:XCTestExpectation = expectationWithDescription("Get AttractionContent of " + String(attractID.ID))
+//            let content:Content = Content()
+//            contentView.receiveID = attractID.ID
+////            contentView.handler = {(response:NSURLResponse!,data:NSData!,error:NSError!) -> Void in
+////                if data != nil {
+////                    var json = JSON(data: data!)
+////                    content.title = json[1]["Title"].stringValue
+////                    content.description = json[1]["Description"].stringValue
+////                    expect.fulfill()
+////                }else{
+////                    XCTFail("Can't get data with some reason.")
+////                }
+////            }
+//            
+//            contentView.viewDidLoad()
+//            waitForExpectationsWithTimeout(5, handler: nil)
+//            
+//            XCTAssertEqual(attractID.description, content.description, "Same description")
+//            XCTAssertEqual(attractID.title, content.title, "Same Title")
+//        }
+//    }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
